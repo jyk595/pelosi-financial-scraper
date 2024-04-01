df1a517051da4ac5ad90e1dcba6e8d7139cf4b8a
John Kim <jyk595@gmail.com>
2024-03-31 19:21:18 -0700

    chrome adjustment

--- scraper.js
+++ scraper.js
@@ -1,12 +1,18 @@
 const nodemailer = require('nodemailer');
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
+const chrome = require('chrome-aws-lambda');
 require('dotenv').config();
 
 const receiveEmail = 'jyk595@gmail.com';
 const today = new Date();
 
 async function scrapeAndSendEmail() {
-    const browser = await puppeteer.launch({executablePath: await puppeteer.executablePath()});
+    const browser = await puppeteer.launch({
+        executablePath: await chrome.executablePath,
+        args: chrome.args,
+        defaultViewport: chrome.defaultViewport,
+        headless: chrome.headless
+    });
     const page = await browser.newPage();
     
     // Load the search page
