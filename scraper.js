85919664396751f801c2988cfae08ad72452408f
John Kim <jyk595@gmail.com>
2024-03-31 20:00:40 -0700

    Updated args & executable path for puppeteer

--- scraper.js
+++ scraper.js
@@ -6,7 +6,17 @@ const receiveEmail = 'jyk595@gmail.com';
 const today = new Date();
 
 async function scrapeAndSendEmail() {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: [
+            '--no-sandbox', 
+            '--disable-setuid-sandbox',
+            '--single-process',
+            '--no-zygote',
+        ],
+        executablePath: process.env.NODE_ENV === 'production' 
+            ? process.env.PUPPETEER_EXECUTABLE_PATH 
+            : puppeteer.executablePath(),
+    });
     const page = await browser.newPage();
     
     // Load the search page
